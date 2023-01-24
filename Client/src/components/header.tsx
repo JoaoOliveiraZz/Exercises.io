68a0fc6a6b579ddff432b71925cf22e34431f57f
JoaoOliveiraZz <linajoaovictor@gmail.com>
2023-01-23 23:28:40 -0300

    Configuração do Tailwindcss

---
+++ Client/src/components/header.tsx
@@ -0,0 +1,13 @@
+
+
+export function Header(){
+
+    return (
+        <div className="h-24 w-full bg-violet-700 flex items-center justify-between p-3"> 
+            <div className="text-zinc-900">
+
+            </div>
+        </div>
+    )
+
+}
---
+++ Client/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
